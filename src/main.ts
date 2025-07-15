3841a95ea428ef3c5b224d472268d362a0204c93
RandyFrias <randy140801@gmail.com>
2025-07-15 14:43:03 -0400

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000', // <-- frontend port
-    credentials: true,
-  });
-  await app.listen(3001); // o el puerto que uses
+  app.enableCors();
+  await app.listen(3001);
 }
 bootstrap();
